cf91231ee208188ef4b44595d54e87c3c4200b84
gartemy <artemganzhaa@gmail.com>
2021-12-18 00:02:58 +0500

    Initialize store

---
+++ store/index.js
@@ -0,0 +1,22 @@
+export const state = {
+  cartItems: []
+}
+
+export const getters = {
+  cartItems: cartItems => state.cartItems
+}
+
+export const mutations = {
+  'ADD_TO_CART' (state, product) {
+    product.selected = true
+    state.cartItems.push(product)
+  },
+  'REMOVE_FROM_CART' (state, product) {
+    product.selected = false
+    state.cartItems = state.cartItems.filter(item => item.id != product.id)
+  },
+  'CLEAR_CART' (state) {
+    state.cartItems.map(item => item.selected = false)
+    state.cartItems = []
+  }
+}
